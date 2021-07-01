6cf5da313dfc8471daf9c38612be4fad420a237e
dzhumabekovait@gmail.com <dzhumabekovait@gmail.com>
2021-07-01 09:24:49 +0600

    add theme

---
+++ lib/theme/text_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'color_theme.dart';
+
+class TextThemes {
+  static var hourlyList = TextStyle(
+    fontWeight: FontWeight.normal,
+    color: ColorPalette.white,
+    //shadows: shadow,
+  );
+}
