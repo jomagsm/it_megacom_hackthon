ce75d6677e6e57e0f2d665229a9d2e94d106f3cc
jomagsm <jomagsm@gmail.com>
2021-07-01 21:37:14 +0600

    Created test models with data

---
+++ lib/data/network/models/buying_model.dart
@@ -0,0 +1,52 @@
+import 'package:it_megacom_hackthon/data/network/models/product_model.dart';
+import 'package:it_megacom_hackthon/data/network/models/user_model.dart';
+
+import 'buying_product_model.dart';
+
+class Buying {
+  DateTime date; // "2017-08-06T00:00:00"
+  UserApp user;
+  List<BuyingProduct> buyingProduct;
+  double total;
+  double howMuchPay;
+  double howMuchDebt;
+
+  Buying({
+    this.date,
+    this.user,
+    this.buyingProduct,
+    this.total,
+    this.howMuchPay,
+    this.howMuchDebt,
+  });
+}
+
+List<Buying> getAllBuying() {
+  List<Buying> buying = [];
+  buying.add(Buying(
+      date: DateTime.parse('2021-07-03T12:30:00'),
+      user: getAllUser().first,
+      buyingProduct: [BuyingProduct(product: getAllProduct().first, qnt: 2)],
+      total: 20,
+      howMuchPay: 20,
+      howMuchDebt: 0));
+  buying.add(Buying(
+      date: DateTime.parse('2021-07-03T12:40:00'),
+      user: getAllUser()[1],
+      buyingProduct: [BuyingProduct(product: getAllProduct()[2], qnt: 1)],
+      total: 45,
+      howMuchPay: 20,
+      howMuchDebt: 25));
+
+  buying.add(Buying(
+      date: DateTime.parse('2021-07-03T13:40:00'),
+      user: getAllUser()[2],
+      buyingProduct: [
+        BuyingProduct(product: getAllProduct()[2], qnt: 1),
+        BuyingProduct(product: getAllProduct()[0], qnt: 3)
+      ],
+      total: 75,
+      howMuchPay: 50,
+      howMuchDebt: 25));
+  return buying;
+}
--- lib/data/network/models/user_model.dart
+++ lib/data/network/models/user_model.dart
@@ -2,28 +2,47 @@ class UserApp {
   int id;
   String firstName;
   String lastName;
-  String fullName;
   double debt;
+  String password;
+  String avatar;
 
-  UserApp({
-    this.id,
-    this.firstName,
-    this.lastName,
-    fullName,
-  });
+  UserApp(
+      {this.id,
+      this.firstName,
+      this.lastName,
+      this.debt,
+      this.password,
+      this.avatar});
 }
 
-class Product {
-  int id;
-  String name;
-  double price;
-  int qnt;
-  Product({
-    this.id,
-    this.name,
-    this.price,
-    this.qnt,
-  });
-}
+List<UserApp> getAllUser() {
+  List<UserApp> usersList = [];
 
-class Cart {}
+  usersList.add(UserApp(
+      id: 1,
+      firstName: 'Tom',
+      lastName: 'Hardy',
+      debt: 1000,
+      password: 'test',
+      avatar:
+          'https://www.nme.com/wp-content/uploads/2018/08/GettyImages-813932476.jpg'));
+
+  usersList.add(UserApp(
+      id: 2,
+      firstName: 'Ilon',
+      lastName: 'Mask',
+      debt: 50,
+      password: 'test',
+      avatar:
+          'https://www.ixbt.com/img/x780/n1/news/2021/4/0/mfile_1569242_1_L_20210108014210_large.png'));
+
+  usersList.add(UserApp(
+      id: 3,
+      firstName: 'Jack',
+      lastName: 'Ma',
+      debt: 777,
+      password: 'test',
+      avatar:
+          'https://c.files.bbci.co.uk/1973/production/_115251560_jackma.jpg'));
+  return usersList;
+}
