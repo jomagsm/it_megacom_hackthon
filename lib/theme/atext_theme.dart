a5bdd8649572090679734ee9da2143f6730c0da3
dzhumabekovait@gmail.com <dzhumabekovait@gmail.com>
2021-07-03 01:23:51 +0600

    add app bar

--- lib/theme/atext_theme.dart
+++ lib/theme/atext_theme.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:it_megacom_hackthon/theme/acolor_theme.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
 class AtextThemes {
-  static TextStyle title = TextStyle(
-      fontSize: 22,
-      fontWeight: FontWeight.normal,
-      color: AcolorPalette.textforgot);
+  static var productsName = GoogleFonts.lato(
+    textStyle: TextStyle(fontSize: 10, color: AcolorPalette.authback),
+  );
   static TextStyle text = TextStyle(
       fontSize: 17,
+      fontWeight: FontWeight.normal,
+      color: AcolorPalette.textforgot);
+  static TextStyle title = TextStyle(
+      fontSize: 22,
       fontWeight: FontWeight.normal,
       color: AcolorPalette.textforgot);
 }
