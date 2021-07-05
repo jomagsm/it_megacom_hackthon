e07ac93b93810cda67861f5f47c1477995eecd60
jomagsm <jomagsm@gmail.com>
2021-07-05 16:41:01 +0600

    refactoring code

--- lib/theme/color_theme.dart
+++ lib/theme/color_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ColorPalette {
   static var white = Colors.white;
   static var blue = Color(0xFF4BAFCF);
-  static var productsCardName = Color.fromRGBO(0, 0, 0, 0.5);
+  static var productsCardName = Color(0xff000000);
   static var productsCardCirculeAvatar = Color.fromRGBO(30, 30, 30, 0.85);
   static var purple = Color(0xFF592A7E);
   static var productsCardSelectedBackground = Color.fromRGBO(30, 30, 30, 0.65);
